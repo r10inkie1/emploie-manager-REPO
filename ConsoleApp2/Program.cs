93a3961782e6089cd58c5362deced6d432a047d4
s6105104 <s6105104@TU42917>
2018-10-16 13:57:38 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n, k;
+            Console.WriteLine("input hower");
+            String hwoer = Console.ReadLine();
+
+             n = double.Parse(hwoer);
+             k = n * 9.50;
+
+            Console.WriteLine("The weekly wage" + k);
+
+            Console.Read();
+
+
+
+
+
+        }
+    }
+}
